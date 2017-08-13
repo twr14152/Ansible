a33fed9f2a68f72c35008ded92a63ad7ec08bb8f
Todd Riemenschneider <twr14152@gmail.com>
2017-08-13 15:01:05 -0400

    Update GNS3_ansible_setup_script.sh

--- GNS3_ansible_setup_script.sh
+++ GNS3_ansible_setup_script.sh
@@ -12,6 +12,13 @@ mkdir /etc/ansible/backups
 
 # Copy first playbook file to playbooks folder
 cp ios_rtr_bkup.yaml /etc/ansible/playbooks/
+cp show_cmds.yaml /etc/ansible/playbooks/
+cp interface_config.yaml /etc/ansible/playbooks/
+
 
 # Update append lab hosts to /etc/ansible/host file
 cat updates_for_hosts >> /etc/ansible/hosts 
+
+# Create director and add ios-rtr.yaml to it
+mkdir /etc/ansible/group_vars
+
